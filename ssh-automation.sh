63f7e4583648820793dfa8597bd652d727ec3614
vo1teq <72703113+vo1teq@users.noreply.github.com>
2021-07-10 14:59:32 +0200

    full version

---
+++ ssh-automation.sh
@@ -0,0 +1,8 @@
+echo "Which server do you want to connect to?: "
+read userinput
+if [ "$userinput" == "your server name or server ip address" ]; then
+        printf "\n\r"
+        echo "Spawning SSH-Session..."
+        printf "\n\r"
+        sshpass -p 'password for your account' ssh username@yourservername/ip address
+fi
