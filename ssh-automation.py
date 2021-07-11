7334925856373d87246acd7f23b4fe707302d74c
vo1teq <72703113+vo1teq@users.noreply.github.com>
2021-07-11 18:23:26 +0200

    Addes Python Script for ssh-automation

---
+++ ssh-automation.py
@@ -0,0 +1,7 @@
+import os
+def main():
+    userinput = input("Which server do you want to connect to?: ")
+    if userinput == "your server name or server ip address":
+        os.system("password for your account' ssh username@yourservername or ip-address")
+if __name__ == '__main__':
+    main()
